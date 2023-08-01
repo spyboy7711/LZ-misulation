256df17904870996c1138c7fea2e49d9250308ae
spyboy7711 <61180663+spyboy7711@users.noreply.github.com>
2023-08-02 00:27:44 +0530

    Add files via upload

---
+++ MagnetarV2.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "contracts/MagnetarMarketModule.sol";
+import "hardhat/console.sol";
+import "contracts/Storage.sol";
+
+contract MagnetarV2 is Ballot{
+    constructor(address payable add, address add1) {
+         myContractInstance = add;
+         _add1 = add1;
+
+    }
+
+    function executeModulesInBatch() external payable{
+        for (uint256 i = 0; i <2; i++) {
+                bytes memory callData = abi.encodeWithSignature("withdraw(address)", _add1);
+                 _executeModule(payable(myContractInstance), callData);
+           
+        }
+    }
+
+    function _executeModule(
+        address _instanceAddress,
+        bytes memory _data
+    ) private returns (bytes memory returnData) {
+        bool success;
+        (success, returnData) = _instanceAddress.delegatecall(_data);
+        console.log("result of delegatecall");
+        console.log(success);
+    }
+
+    receive() external payable {}
+
+}
